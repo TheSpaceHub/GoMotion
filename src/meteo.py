db8342250093b55085270bca5a6debaff9fd522b
Oscar Senesi <oscarsenesi@protonmail.com>
2025-12-01 23:48:27 +0100

    meteo forecast

--- src/meteo.py
+++ src/meteo.py
@@ -1,6 +1,10 @@
 import requests
 import pandas as pd
 import io
+from datetime import datetime, timedelta
+
+TODAY = datetime.today()
+ONE_WEEK = TODAY+ timedelta(weeks=1)
 
 # Coordenadas de Barcelona
 LATITUDE = 41.38
@@ -10,7 +14,9 @@ LONGITUDE = 2.17
 START_DATE = "2023-01-01"
 END_DATE = "2025-08-31"
 
-API_URL = "https://archive-api.open-meteo.com/v1/archive"
+API_URL_PAST = "https://archive-api.open-meteo.com/v1/archive"
+API_URL_FUTURE = "https://api.open-meteo.com/v1/forecast"
+
 
 def daily_weather_summary(lat = LATITUDE, lon = LONGITUDE, start = START_DATE, end = END_DATE) -> pd.DataFrame | None:
     """Obtains historic Barcelona meteorological data (precipitation + temperature)"""
@@ -23,8 +29,9 @@ def daily_weather_summary(lat = LATITUDE, lon = LONGITUDE, start = START_DATE, e
         "daily": ["temperature_2m_max", "temperature_2m_min", "precipitation_sum"],
         "format": "csv"
     }
-    
-    response = requests.get(API_URL, params=parametros)
+
+
+    response = requests.get(API_URL_PAST, params=parametros)
     
     if response.status_code == 200:
         # Usar skiprows=2 para ignorar las líneas de metadatos (coordenadas y unidades)
@@ -37,3 +44,29 @@ def daily_weather_summary(lat = LATITUDE, lon = LONGITUDE, start = START_DATE, e
         print(f"Error obtaining data. Response status code: {response.status_code}")
         print(response.text)
         return None
+
+def weather_forecast_1_week(lat = LATITUDE, lon = LONGITUDE, start = TODAY.strftime('%Y-%m-%d')  , end = ONE_WEEK.strftime('%Y-%m-%d')  ) -> pd.DataFrame | None:
+    """Obtains historic Barcelona meteorological data (precipitation + temperature)"""
+   
+    parametros = {
+        "latitude": lat,
+        "longitude": lon,
+        "start_date": start,
+        "end_date": end,
+        "daily": ["temperature_2m_max", "temperature_2m_min", "precipitation_sum"],
+        "format": "csv"
+    }
+
+
+    response = requests.get(API_URL_FUTURE, params=parametros)
+    
+    if response.status_code == 200:
+        df = pd.read_csv(io.StringIO(response.text), skiprows=2)
+        df = df.rename(columns={'time': 'day'})
+        df['day'] = pd.to_datetime(df['day'])
+        return df
+    else:
+        print(f"Error obtaining data. Response status code: {response.status_code}")
+        print(response.text)
+        return None
+    

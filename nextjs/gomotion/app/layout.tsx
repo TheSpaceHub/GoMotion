777b9c59fa569acf07c4df16268a58512b95ffbe
Dan Vancea <100865899+TheSpaceHub@users.noreply.github.com>
2026-02-13 19:59:51 +0100

    Improve SEO

--- nextjs/gomotion/app/layout.tsx
+++ nextjs/gomotion/app/layout.tsx
@@ -1,10 +1,26 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-
 export const metadata: Metadata = {
   title: "GoMotion",
-  description: "Mobility peak detection in Barcelona.",
+  description:
+    "Analyze and predict traffic trends, event impacts, and mobility statistics in Barcelona.",
+  keywords: ["Barcelona", "GoMotion", "mobility", "prediction", "dashboard"],
+  openGraph: {
+    title: "GoMotion",
+    description: "Mobility peak detection in Barcelona.",
+    url: "https://go-motion.vercel.app",
+    siteName: "GoMotion",
+    images: [
+      {
+        url: "https://go-motion.vercel.app/GoMotionPreview.png",
+        width: 681,
+        height: 562,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
@@ -17,8 +33,7 @@ export default function RootLayout({
       <head>
         <title>GoMotion</title>
       </head>
-      <body>{children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }

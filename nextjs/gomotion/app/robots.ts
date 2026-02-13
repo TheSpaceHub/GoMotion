c7e4d3aefc613171fcb036a1346f6dcc389f8744
Dan Vancea <100865899+TheSpaceHub@users.noreply.github.com>
2026-02-13 20:14:47 +0100

    Allow bot crawling
    sorry first time doing seo

---
+++ nextjs/gomotion/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://go-motion.vercel.app/sitemap.xml",
+  };
+}
